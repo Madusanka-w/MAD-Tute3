9bc0674a3e8204a56f5967456bef8c75db4e1c2f
Madusanka-w <mwattale@gmail.com>
2021-08-10 13:49:59 +0530

    Creating custom toast.

--- app/src/main/java/com/example/intentsproj/MainActivity.java
+++ app/src/main/java/com/example/intentsproj/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +26,20 @@ public class MainActivity extends AppCompatActivity {
         tx_num1 = findViewById(R.id.tx_one);
         tx_num2 = findViewById(R.id.tx_two);
         btn_ok = findViewById(R.id.btn_ok);
+
+            setContentView(R.layout.activity_main);
+            //Creating the LayoutInflater instance
+            LayoutInflater li = getLayoutInflater();
+            //Getting the View object as defined in the customtoast.xml file
+            View layout = li.inflate(R.layout.customtoast, (ViewGroup)
+                    findViewById(R.id.custom_toast_layout));
+            //Creating the Toast object
+            Toast toast = new Toast(getApplicationContext());
+            toast.setDuration(Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.setView(layout);//setting the view of custom toast layout
+            toast.show();
+
 
     }
 
